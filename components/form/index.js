eb56df55f3397498918a989fa4a5ea031841f6f9
Holden Budiman <holdenbudiman@Holdens-MacBook-Pro.local>
2024-01-12 23:23:49 -0800

    adding updated code

--- components/form/index.js
+++ components/form/index.js
@@ -1,5 +1,6 @@
 import styles from './form.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Form() {
     return(
@@ -22,21 +23,21 @@ export default function Form() {
                         placeholder="password"
                         />
                         <button className={styles.loginButton}>Log In</button>
-                        <div id="or">
-                            <p>OR</p>
+                        <div className={styles.orContainer}>
+                        <div className={styles.or}><span> OR </span></div>
                         </div>
-                        <a href="#" className={styles.facebookLogin}>Log in with Facebook</a>
-                        <a href="#" className={styles.forgotPassword}>Forgot Password?</a>
+                        <Link href="#" className={styles.facebookLogin}>Log in with Facebook</Link>
+                        <Link href="#" className={styles.forgotPassword}>Forgot Password?</Link>
                 </form>
                 <div className={styles.signUp}>
-                    <p className='signUpLink'>Don't have an account? <a href="#" className={styles.signUpLink}>Sign up</a></p>
+                    <p className='signUpLink'>  Don't have an account?   <Link href="#" className={styles.signUpLink}>Sign up</Link></p>
                 </div>
                 <div className={styles.getApp}>
                     <p className={styles.getAppText}>Get the app.</p>
             </div>
-            <div>
-            <Image src={'/images/downloadbtn.png'} alt="download" width={136} height={40} className='downloadBtn'/>
-            <Image src={'/images/googleplaybtn.png'} alt="google play" width={155} height={58} className='googlePlay'/>
+            <div className={styles.downloadImgs}>
+            <Image href="#" src={'/images/google-play.png'} alt="download" width={135} height={44} className='downloadBtn'/>
+            <Image src={'/images/app-store.png'} alt="google play" width={135} height={42} className='googlePlay'/>
             </div>
         </div>
         </div>
